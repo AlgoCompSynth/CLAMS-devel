ae1ca76c26003f44ddcff7bac56c30010165112e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-11-10 16:01:11 -0800

    add Adafruit fork of Teensy audio

--- arduino_ide/32_adafruit.sh
+++ arduino_ide/32_adafruit.sh
@@ -9,19 +9,9 @@ export LOGFILE=$PWD/32_adafruit.sh.log
 rm --force $LOGFILE
 
 echo "Installing Adafruit libraries"
-arduino-cli lib install "Adafruit SPIFlash" \
-  >> $LOGFILE 2>&1
-arduino-cli lib install "Adafruit TinyFlash" \
-  >> $LOGFILE 2>&1
 arduino-cli lib install "Adafruit TinyUSB Library" \
   >> $LOGFILE 2>&1
-arduino-cli lib install "Adafruit WavePlayer Library" \
-  >> $LOGFILE 2>&1
 arduino-cli lib install "Audio - Adafruit Fork" \
-  >> $LOGFILE 2>&1
-arduino-cli lib install "PicoDVI - Adafruit Fork" \
-  >> $LOGFILE 2>&1
-arduino-cli lib install "usb_midi_host" \
   >> $LOGFILE 2>&1
 
 # https://github.com/pschatzmann/Adafruit_TinyUSB_Arduino/discussions/1
