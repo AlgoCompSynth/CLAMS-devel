fb798572d86d7507469c205465bf308a83d77464
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-11-28 16:33:10 -0800

    ditch the GitHub piece

--- arduino/adafruit.sh
+++ arduino/adafruit.sh
@@ -12,17 +12,7 @@ export LOGFILE=$PWD/Logs/adafruit.log
 rm --force $LOGFILE
 
 echo "Installing Adafruit libraries"
-arduino-cli lib install "Adafruit TinyUSB Library" \
-  >> $LOGFILE 2>&1
 arduino-cli lib install "Audio - Adafruit Fork" \
   >> $LOGFILE 2>&1
-
-# https://github.com/pschatzmann/Adafruit_TinyUSB_Arduino/discussions/1
-echo "Installing Adafruit_TinyUSB_Arduino from GitHub"
-pushd $ADAFRUIT_TINYUSB_ARDUINO_PATH/..
-  rm --force --recursive $ADAFRUIT_TINYUSB_ARDUINO_PATH
-  /usr/bin/time git clone $ADAFRUIT_TINYUSB_ARDUINO_URL \
-    >> $LOGFILE 2>&1
-popd
 
 echo "Finished"
