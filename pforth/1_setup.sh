a6308a927e102f128a4506eff61a84515cbc6d60
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-03-31 12:02:33 -0700

    Host pforth builder

---
+++ pforth/1_setup.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo ""
+echo "Setting environment variables"
+source ../set_pico_envars
+export LOGFILE="$PWD/1_setup.log"
+rm --force $LOGFILE
+
+echo "Cloning fresh pforth project repository"
+mkdir --parents $PFORTH_PATH
+pushd $PFORTH_PATH/..
+  rm --force --recursive $PFORTH_PATH
+  /usr/bin/time git clone $PFORTH_URL \
+    >> $LOGFILE 2>&1
+popd
+
+pushd $PFORTH_PATH
+
+  echo "Building pforth"
+  date +"%F %T" \
+    >> $LOGFILE 2>&1
+  cmake . \
+    >> $LOGFILE 2>&1
+  /usr/bin/time make \
+    >> $LOGFILE 2>&1
+  cp fth/pforth_standalone $HOME/.local/bin/
+  ln -s $HOME/.local/bin/pforth_standalone $HOME/.local/bin/pforth
+  date +"%F %T" \
+    >> $LOGFILE 2>&1
+
+popd
+
+echo "Finished"
