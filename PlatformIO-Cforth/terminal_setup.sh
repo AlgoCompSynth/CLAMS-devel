278fc393ba636e7aa327e2093534c40adaf2399d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-04-26 20:09:36 -0700

    PlatformIO-Cforth consolidation done

---
+++ PlatformIO-Cforth/terminal_setup.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo "Creating $HOME/.local/bin and $HOME/bin"
+mkdir --parents $HOME/.local/bin
+mkdir --parents $HOME/bin
+
+echo "Setting base configuration files"
+cp bashrc $HOME/.bashrc; source bashrc
+cp bash_aliases $HOME/.bash_aliases; source bash_aliases
+cp vimrc $HOME/.vimrc
+
+echo "Installing Meslo nerd fonts"
+pushd /tmp
+
+  echo "Downloading patched MesloLG Nerd fonts"
+  rm --force --recursive Meslo*
+  curl -sOL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip
+  mkdir Meslo
+  cd Meslo
+  unzip ../Meslo.zip
+
+  echo "Copying to $HOME/.fonts"
+  mkdir --parents $HOME/.fonts
+  cp *.ttf $HOME/.fonts
+
+  popd
+
+echo "Installing Starship"
+./install_starship.sh
+mkdir --parents $HOME/.config
+cp starship.toml $HOME/.config/starship.toml
+
+echo "Adding Starship prompt to bash"
+echo 'eval "$(starship init bash)"' >> $HOME/.bashrc
+
+echo ""
+echo "Restart bash to get new Starship prompt"
+
+echo "Finished"
