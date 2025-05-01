bc5775369294add768734cebf3dc215eab64e30c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-05-01 00:28:12 -0700

    add Linux dependencies

--- PlatformIO-Cforth/1_install_all.sh
+++ PlatformIO-Cforth/1_install_all.sh
@@ -4,5 +4,6 @@ set -e
 
 ./terminal_setup.sh
 ./apt_basic_devel.sh
+./linux_dependencies.sh
 ./platformio_cforth_base.sh
 ./apt_pkg_db_updates.sh
---
+++ PlatformIO-Cforth/linux_dependencies.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo "Defining LOGFILE"
+mkdir --parents $PWD/Logs
+export LOGFILE=$PWD/Logs/linux_dependencies.log
+rm --force $LOGFILE
+
+echo "Installing Linux build dependencies"
+export DEBIAN_FRONTEND=noninteractive
+/usr/bin/time sudo apt-get install -qqy --no-install-recommends \
+  gcc-multilib \
+  >> $LOGFILE 2>&1
+
+echo "Finished"
