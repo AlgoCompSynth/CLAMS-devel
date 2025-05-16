cb9c56f1901170cf188f8859077cad8e51c7714c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-05-16 16:56:12 -0700

    core-image-full-cmdline aarch64 test results

--- Yocto/test_qemu_arm64.sh
+++ Yocto/test_qemu_arm64.sh
@@ -6,24 +6,25 @@ echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
 mkdir --parents $PWD/Logs
-export LOGFILE=$PWD/Logs/test_qemu.log
+export LOGFILE=$PWD/Logs/test_qemu_arm64.log
 rm --force $LOGFILE
 
-export NEW_CONFIG_FILE=$PWD/test_qemu.conf
+export NEW_CONFIG_FILE=$PWD/test_qemu_arm64.conf
 
 pushd $POKY_PATH
   echo "Initializing build directory"
-  source oe-init-build-env test_qemu_build
+  source oe-init-build-env test_qemu_arm64_build
 
-  echo "Enabling artifact fetch"
+  echo "Setting configuration"
   diff $NEW_CONFIG_FILE conf/local.conf || true
   cp $NEW_CONFIG_FILE conf/local.conf
 
   echo "Building core-image-full-cmdline"
-  bitbake core-image-full-cmdline
+  /usr/bin/time bitbake core-image-full-cmdline \
+    2>&1 | tee --append $LOGFILE
 
   echo "Testing core-image-full-cmdline"
-  runqemu qemux86-64
+  runqemu qemuarm64
 popd
 
 echo "Finished"
