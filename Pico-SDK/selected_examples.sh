c2faa0173954324073dd6e4f1137a6d486001692
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-05-18 16:32:38 -0700

    more "hello_" examples

--- Pico-SDK/selected_examples.sh
+++ Pico-SDK/selected_examples.sh
@@ -21,11 +21,9 @@ pushd $PICO_EXAMPLES_PATH
   do
     /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "blink.$suffix"` \
       >> $LOGFILE 2>&1
-    /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "flash_nuke.$suffix"` \
+    /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "hello_*.$suffix"` \
       >> $LOGFILE 2>&1
-    /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "hello_serial.$suffix"` \
-      >> $LOGFILE 2>&1
-    /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "hello_usb.$suffix"` \
+    /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "flash_*.$suffix"` \
       >> $LOGFILE 2>&1
     /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "tinyusb*.$suffix"` \
       >> $LOGFILE 2>&1
@@ -35,6 +33,8 @@ popd
 pushd $PICO_PLAYGROUND_PATH
   for suffix in uf2 elf dis
   do
+    /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "hello_*.$suffix"` \
+      >> $LOGFILE 2>&1
     /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "sine_wave*.$suffix"` \
       >> $LOGFILE 2>&1
   done
