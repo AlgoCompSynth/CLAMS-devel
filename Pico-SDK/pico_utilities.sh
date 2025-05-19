b3548da7e8137154ff58356960628d2ed4c31c40
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-05-19 12:29:16 -0700

    move utility source to ~/Projects

--- Pico-SDK/pico_utilities.sh
+++ Pico-SDK/pico_utilities.sh
@@ -11,49 +11,51 @@ mkdir --parents "$PWD/Logs"
 export LOGFILE="$PWD/Logs/pico_utilities.log"
 rm --force $LOGFILE
 
-pushd $PICO_SDK_REPOS
-
-  echo ""
+mkdir --parents $OPENOCD_PATH
+echo ""
+pushd $OPENOCD_PATH/..
   echo "Cloning openocd"
   rm --force --recursive $OPENOCD_PATH
   git clone --quiet $OPENOCD_URL
-  pushd $OPENOCD_PATH
-    echo "Building openocd"
-    ./bootstrap \
-      >> $LOGFILE 2>&1
-    ./configure \
-      --enable-ftdi --enable-sysfsgpio --enable-bcm2835gpio --disable-werror \
-      >> $LOGFILE 2>&1
-    /usr/bin/time make -j`nproc` \
-      >> $LOGFILE 2>&1
-    echo "Installing openocd as 'root'!!"
-    sudo make install \
-      >> $LOGFILE 2>&1
-    openocd --version
-  popd
+  cd $OPENOCD_PATH
+  echo "Building openocd"
+  ./bootstrap \
+    >> $LOGFILE 2>&1
+  ./configure \
+    --enable-ftdi --enable-sysfsgpio --enable-bcm2835gpio --disable-werror \
+    >> $LOGFILE 2>&1
+  /usr/bin/time make -j`nproc` \
+    >> $LOGFILE 2>&1
+  echo "Installing openocd as 'root'!!"
+  sudo make install \
+    >> $LOGFILE 2>&1
+  openocd --version
+popd > /dev/null
 
-  echo ""
+mkdir --parents $PICOTOOL_PATH
+echo ""
+pushd $PICOTOOL_PATH/..
   echo "Cloning picotool"
   rm --force --recursive $PICOTOOL_PATH
   git clone --quiet $PICOTOOL_URL
-  pushd $PICOTOOL_PATH
-    echo "Building picotool"
-    mkdir build
-    cd build
-    cmake ../ \
-      >> $LOGFILE 2>&1
-    /usr/bin/time make -j`nproc` \
-      >> $LOGFILE 2>&1
-    echo "Installing picotool as 'root'!!"
-    sudo make install \
-      >> $LOGFILE 2>&1
-    picotool version
-  popd
+  cd $PICOTOOL_PATH
+  echo "Building picotool"
+  mkdir build
+  cd build
+  cmake ../ \
+    >> $LOGFILE 2>&1
+  /usr/bin/time make -j`nproc` \
+    >> $LOGFILE 2>&1
+  echo "Installing picotool as 'root'!!"
+  sudo make install \
+    >> $LOGFILE 2>&1
+  picotool version
+popd > /dev/null
 
-  echo ""
+echo ""
+pushd $PICO_SDK_REPOS
   echo "Downloading Debug Probe firmware"
   curl -sOL $DEBUG_PROBE_FIRMWARE
-
-popd
+popd > /dev/null
 
 echo "Finished"
