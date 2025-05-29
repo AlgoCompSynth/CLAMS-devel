39cbc8d9d9cf9d7d43f95b49ba5158b49894634d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-05-29 16:51:58 -0700

    more refactoring

---
+++ PlatformIO-Cforth/2_dbx_install_all.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+set -e
+
+./platformio_cforth_base.sh
+../apt_pkg_db_updates.sh
--- host_recreate_container.sh
+++ host_recreate_container.sh
@@ -5,15 +5,25 @@ set -e
 echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
-export CTN_NAME=$BASE_CONTAINER_NAME
+export CTN_NAME=`basename "$(pwd)"`
 echo "CTN_NAME: $CTN_NAME"
-export CTN_HOME=$BASE_CONTAINER_HOME
+export CTN_HOME=$DBX_CONTAINER_HOME_PREFIX/$CTN_NAME
 echo "CTN_HOME: $CTN_HOME"
 
+echo "Stopping base container"
+distrobox stop --yes $BASE_CONTAINER_NAME || true
 echo "Stopping any existing container"
 distrobox stop --yes $CTN_NAME || true
 echo "Removing any existing container home"
 rm --force --recursive $CTN_HOME
+echo "Copying base container home"
+cp -rp $BASE_CONTAINER_HOME $CTN_HOME
+
+echo "Creating distrobox.ini"
+sed "s/BASE_CONTAINER_NAME/$BASE_CONTAINER_NAME/" ../distrobox.template \
+  | sed "s/CTN_NAME/$CTN_NAME/" \
+  > distrobox.ini
+cat additional_packages >> distrobox.ini
 
 echo "Creating and starting distrobox container"
 echo "This will take some time to install basic packages"
