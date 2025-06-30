8e22d05c8186fdce9faf043c26e0052b3159c0d7
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-06-30 11:35:07 -0700

    Logitech USB rules

--- 0_host_udev_rules/host_install_udev_rules.sh
+++ 0_host_udev_rules/host_install_udev_rules.sh
@@ -15,6 +15,9 @@ sudo cp 99-picotool.rules $SYSTEM_UDEV_PATH/
 echo "Installing Arduino MBED RP2040 udev rules as 'root'!!"
 sudo cp 60-arduino-mbed.rules $SYSTEM_UDEV_PATH/
 
+echo "Installing Logitech USB receiver udev rules as 'root'!!"
+sudo cp 77-logitech-usb.rules $SYSTEM_UDEV_PATH/
+
 echo "Installing Zephyr OS openocd udev rules as 'root'!!"
 # https://docs.zephyrproject.org/latest/develop/beyond-GSG.html#setting-udev-rules
 sudo cp 60-openocd.rules $SYSTEM_UDEV_PATH/
