d86d115ddbc031d383d2e1e8231ad3e3a678e79c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-12-15 17:44:53 -0800

    replace c3 and c4 with c4a

--- Linux-Tools/c4a/1_setup.sh
+++ Linux-Tools/c4a/1_setup.sh
@@ -11,30 +11,30 @@ rm --force $LOGFILE
 
 echo "Installing Linux dependencies"
 sudo apt-get install -qqy --no-install-recommends \
+  lib32stdc++-13-dev \
   >> $LOGFILE 2>&1
 
-echo "Cloning fresh c4 project repository"
-mkdir --parents $C4_PATH
-pushd $C4_PATH/..
-  rm --force --recursive $C4_PATH
-  /usr/bin/time git clone $C4_URL \
+echo "Cloning fresh c4a project repository"
+mkdir --parents $C4A_PATH
+pushd $C4A_PATH/..
+  rm --force --recursive $C4A_PATH
+  /usr/bin/time git clone $C4A_URL \
     >> $LOGFILE 2>&1
 popd
 
-pushd $C4_PATH
+pushd $C4A_PATH
 
-  echo "Building c4"
+  echo "Building c4a"
   date +"%F %T" \
     >> $LOGFILE 2>&1
-  export ARCH=64
   /usr/bin/time make \
     >> $LOGFILE 2>&1
   date +"%F %T" \
     >> $LOGFILE 2>&1
 
-popd
+  echo "Installing c4a"
+  cp c4a $HOME/.local/bin
 
-echo "Installing c4"
-cp c4 $HOME/.local/bin
+popd
 
 echo "Finished"
