d07460130cefc0d675d9842d62ec33215ba83809
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-12-18 17:18:49 -0800

    buildroot install tested - may drop later

---
+++ Buildroot/1_install_all.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+set -e
+
+#! /usr/bin/env bash
+
+set -e
+
+echo ""
+echo "Setting environment variables"
+source ../set_pico_envars
+
+distrobox enter "$DBX_CONTAINER_NAME" -- ./linux_dependencies.sh
+distrobox enter "$DBX_CONTAINER_NAME" -- ./install_buildroot.sh
+distrobox enter "$DBX_CONTAINER_NAME" -- ./apt_pkg_db_updates.sh
