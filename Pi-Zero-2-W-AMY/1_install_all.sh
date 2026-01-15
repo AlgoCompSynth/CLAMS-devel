9fc97d4bf8a7703a8b1aace12758e2a5df493c82
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-01-14 18:54:22 -0800

    base working - next step is AMY

--- Pi-Zero-2-W-AMY/1_install_all.sh
+++ Pi-Zero-2-W-AMY/1_install_all.sh
@@ -2,22 +2,11 @@
 
 set -e
 
-echo ""
-echo "Setting environment variables"
-source ../set_pico_envars
-
 for option in \
-  c4a \
-  cforth \
-  pforth \
-  zForth \
-  gforth
+  terminal_setup \
+  c4
 do
   pushd $option
-    distrobox enter "$DBX_CONTAINER_NAME" -- ./1_setup.sh
+    ./1_setup.sh
   popd
 done
-
-distrobox enter "$DBX_CONTAINER_NAME" -- ./linux_circuitpython.sh
-distrobox enter "$DBX_CONTAINER_NAME" -- ./linux_micropython.sh
-distrobox enter "$DBX_CONTAINER_NAME" -- ./apt_pkg_db_updates.sh
